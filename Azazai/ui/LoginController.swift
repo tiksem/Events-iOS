b55c765e07038d0ddbaa33c2f8f1dd63222f5f81
Semen Tykhonenko <stykhonenko@cogniance.com>
2016-01-28 17:37:49 +0200

    login screen implementation started

---
+++ Azazai/ui/LoginController.swift
@@ -0,0 +1,14 @@
+//
+// Created by Semyon Tikhonenko on 1/28/16.
+// Copyright (c) 2016 ___FULLUSERNAME___. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SwiftUtils
+
+class LoginController : UIViewController {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder);
+    }
+}
