ac3a48f4b8ed84fd980d2023ce0b31ca7165e873
Semen Tykhonenko <stykhonenko@cogniance.com>
2016-01-29 23:35:12 +0200

    Add vk sdk

--- Azazai/AppDelegate.swift
+++ Azazai/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 
 import UIKit
+import VKSdkFramework
 
 
 @UIApplicationMain
@@ -40,6 +41,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func applicationWillEnterForeground(application: UIApplication) {
+        VKSdk.accessToken()
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
---
+++ Azazai/Azazai-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Azazai-Bridging-Header.h
+//  Azazai
+//
+//  Created by Semyon Tikhonenko on 1/29/16.
+//  Copyright © 2016 Semyon Tikhonenko. All rights reserved.
+//
+
+#ifndef Azazai_Bridging_Header_h
+#define Azazai_Bridging_Header_h
+
+#import <VKSdkFramework/VKSdkFramework.h>
+
+#endif /* Azazai_Bridging_Header_h */
