91012f0604166a6078e1a12cf31d9f4cfc9e668e
Semen Tykhonenko <stykhonenko@cogniance.com>
2016-01-30 20:46:18 +0200

    vk basic login logic implemented

--- Azazai/ui/LoginController.swift
+++ Azazai/ui/LoginController.swift
@@ -7,12 +7,57 @@ import Foundation
 import UIKit
 import SwiftUtils
 
-class LoginController : UIViewController {
+class LoginController : UIViewController, VKSdkDelegate, VKSdkUIDelegate {
+    let PERMISSIONS:[AnyObject] = [VK_PER_OFFLINE, VK_PER_WALL]
+    var onViewDidAppear:(() -> Void)?
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder);
+        VKSdk.instance().registerDelegate(self)
+        VKSdk.instance().uiDelegate = self
     }
     
     @IBAction func onLoginClick(sender: UIButton) {
+        VKSdk.authorize(PERMISSIONS)
+    }
+
+    func onLoginSuccess() {
+        showEvents()
+    }
+
+    func showEvents() {
         performSegueWithIdentifier("ShowEvents", sender: self)
+    }
+
+    func vkSdkAccessAuthorizationFinishedWithResult(_ result: VKAuthorizationResult!) {
+        onViewDidAppear = {
+            if let error = result.error {
+                Alerts.showOkAlert(error.description)
+            } else {
+                self.onLoginSuccess()
+            }
+        }
+    }
+
+    func vkSdkUserAuthorizationFailed() {
+    }
+
+    func vkSdkShouldPresentViewController(_ controller: UIViewController!) {
+        presentViewController(controller, animated: true, completion: nil)
+    }
+
+    func vkSdkNeedCaptchaEnter(_ captchaError: VKError!) {
+        print("vkSdkNeedCaptchaEnter")
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        onViewDidAppear?()
+        onViewDidAppear = nil
+    }
+
+
+    deinit {
+        VKSdk.instance().unregisterDelegate(self)
     }
 }
