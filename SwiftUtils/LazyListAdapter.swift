474989dae4e3b8b7fec72527726b3e8b3b6e0578
Semen Tykhonenko <stykhonenko@cogniance.com>
2016-02-17 08:57:39 +0200

    RandomAccessibleAdapter introduced

--- SwiftUtils/LazyListAdapter.swift
+++ SwiftUtils/LazyListAdapter.swift
@@ -1,24 +1,16 @@
 //
-// Created by Semyon Tikhonenko on 1/10/16.
+// Created by Semyon Tikhonenko on 2/17/16.
 // Copyright (c) 2016 ___FULLUSERNAME___. All rights reserved.
 //
 
 import Foundation
 import UIKit
 
-public class LazyListAdapter<T : Hashable, Error : ErrorType, CellType: UITableViewCell> : NSObject,
-        UITableViewDelegate, UITableViewDataSource {
-    private let cellIdentifier:String
-    private let cellNibFileName:String
-    private let nullCellIdentifier:String
-    private let nullCellNibFileName:String
-    private let list:LazyList<T, Error>
-    private let displayItem:(T, CellType) -> Void
-    private let displayNullItem:((UITableViewCell) -> Void)?
-    private let onItemSelected:((T, Int) -> Void)?
-    private unowned let tableView:UITableView
-
-    public init(cellIdentifier:String,
+public class LazyListAdapter<T:Hashable,
+                            Error:ErrorType,
+                            CellType:UITableViewCell> :
+        RandomAccessableAdapter<LazyList<T, Error>, CellType> {
+    public override init(cellIdentifier:String,
                 cellNibFileName:String? = nil,
                 nullCellIdentifier:String,
                 nullCellNibFileName:String? = nil,
@@ -27,54 +19,19 @@ public class LazyListAdapter<T : Hashable, Error : ErrorType, CellType: UITableV
                 displayNullItem:((UITableViewCell) -> Void)? = nil,
                 onItemSelected:((T, Int) -> Void)? = nil,
                 tableView:UITableView) {
-        self.cellIdentifier = cellIdentifier
-        self.cellNibFileName = cellNibFileName ?? cellIdentifier
-        self.nullCellIdentifier = nullCellIdentifier
-        self.nullCellNibFileName = nullCellNibFileName ?? nullCellIdentifier
-        self.list = list
-        self.displayItem = displayItem
-        self.displayNullItem = displayNullItem
-        self.tableView = tableView
-        self.onItemSelected = onItemSelected
+        super.init(cellIdentifier: cellIdentifier,
+                cellNibFileName: cellNibFileName,
+                nullCellIdentifier: nullCellIdentifier,
+                nullCellNibFileName: nullCellNibFileName,
+                list: list,
+                displayItem: displayItem,
+                displayNullItem: displayNullItem,
+                onItemSelected: onItemSelected,
+                tableView: tableView)
 
-        UiUtils.registerNib(tableView: tableView, nibName: self.cellNibFileName, cellIdentifier: cellIdentifier)
-        UiUtils.registerNib(tableView: tableView, nibName: self.nullCellNibFileName, cellIdentifier: nullCellIdentifier)
-
-        super.init()
-
-        reloadData()
         list.onNewPageLoaded = {
             (data) in
             self.reloadData()
-        }
-    }
-
-    private func reloadData() {
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.reloadData()
-    }
-
-    public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count
-    }
-
-    public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        if let item = list[indexPath.row] {
-            let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as! CellType
-            displayItem(item, cell)
-            return cell
-        } else {
-            let cell = tableView.dequeueReusableCellWithIdentifier(nullCellIdentifier)!
-            displayNullItem?(cell)
-            return cell
-        }
-    }
-
-    public func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let row = indexPath.row
-        if let item = list[row] {
-            onItemSelected?(item, row)
         }
     }
 }
---
+++ SwiftUtils/RandomAccessibleAdapter.swift
@@ -0,0 +1,79 @@
+//
+// Created by Semyon Tikhonenko on 1/10/16.
+// Copyright (c) 2016 ___FULLUSERNAME___. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class RandomAccessibleAdapter<Container:RandomAccessable,
+                                    CellType:UITableViewCell> :
+        NSObject, UITableViewDelegate, UITableViewDataSource {
+    public typealias T = Container.ItemType
+
+    private let cellIdentifier:String
+    private let cellNibFileName:String
+    private let nullCellIdentifier:String
+    private let nullCellNibFileName:String
+    private let list:Container
+    private let displayItem:(T, CellType) -> Void
+    private let displayNullItem:((UITableViewCell) -> Void)?
+    private let onItemSelected:((T, Int) -> Void)?
+    private unowned let tableView:UITableView
+
+    public init(cellIdentifier:String,
+                cellNibFileName:String? = nil,
+                nullCellIdentifier:String,
+                nullCellNibFileName:String? = nil,
+                list:Container,
+                displayItem:(T, CellType) -> Void,
+                displayNullItem:((UITableViewCell) -> Void)? = nil,
+                onItemSelected:((T, Int) -> Void)? = nil,
+                tableView:UITableView) {
+        self.cellIdentifier = cellIdentifier
+        self.cellNibFileName = cellNibFileName ?? cellIdentifier
+        self.nullCellIdentifier = nullCellIdentifier
+        self.nullCellNibFileName = nullCellNibFileName ?? nullCellIdentifier
+        self.list = list
+        self.displayItem = displayItem
+        self.displayNullItem = displayNullItem
+        self.tableView = tableView
+        self.onItemSelected = onItemSelected
+
+        UiUtils.registerNib(tableView: tableView, nibName: self.cellNibFileName, cellIdentifier: cellIdentifier)
+        UiUtils.registerNib(tableView: tableView, nibName: self.nullCellNibFileName, cellIdentifier: nullCellIdentifier)
+
+        super.init()
+
+        reloadData()
+    }
+
+    public func reloadData() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.reloadData()
+    }
+
+    public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+
+    public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if let item = list[indexPath.row] {
+            let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as! CellType
+            displayItem(item, cell)
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCellWithIdentifier(nullCellIdentifier)!
+            displayNullItem?(cell)
+            return cell
+        }
+    }
+
+    public func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let row = indexPath.row
+        if let item = list[row] {
+            onItemSelected?(item, row)
+        }
+    }
+}
